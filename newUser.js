c4e3afc3b44f94b684e54bdf8ee096b060dc6963
sujeet_1miles <sujeet74560@gmail.com>
2023-12-21 15:00:13 +0530

    minipost

--- newUser.js
+++ newUser.js
@@ -1,7 +1,9 @@
 let express=require("express");
+let cors = require("cors");
 let app = express();
 let axios = require("axios");
 app.use(express.json());
+app.use(cors());
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header(
