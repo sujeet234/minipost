b7b6a9201f541223ad02c29b8a1049f598fa39fc
sujeet_1miles <sujeet74560@gmail.com>
2023-12-21 12:47:43 +0530

    minipost

---
+++ newUser.js
@@ -0,0 +1,72 @@
+let express=require("express");
+let cors = require("cors");
+let app = express();
+app.use(express.json());
+app.use(cors());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,POST,OPTIONS,PUT,PATCH,DELETE,HEAD"
+        );
+        res.header("Access-Control-Expose-Headers", "Authorization");
+        res.header(
+            "Access-Control-Allow-Header",
+            "Origin, X-Requested-With,Content-Type,Accept"
+        );
+        next();
+});
+//process.env.PORT ||
+const port =  2410;
+app.listen(port,()=>console.log(`Listening on port ${port}`));
+
+let axios = require("axios");
+
+let urls = [];
+
+app.get("/myServer",function(req,res){
+    // let baseURL = "https://jsonplaceholder.typicode.com";
+    let baseURL = urls.map((el)=>el.url);
+    axios.get(baseURL).then((response)=>{
+        let {data} = response;
+        // console.log("myServer get ",data);
+        res.send(data);
+    })   
+    .catch((err)=>{
+        if(err.response){
+        let {status,statusText} = err.response;
+        // console.log(err.response);
+        res.status(status).send(statusText);
+        }
+        else {
+            res.status(404).send(err);
+        }
+    })
+})
+  
+app.post("/urls",function(req,res){
+    let body = req.body;
+    urls.push({method:body.method,url:body.url});
+    console.log("new urls",urls);    
+})
+
+
+app.post("/myServer",function(req,res){
+    let body = req.body.data;
+    let baseURL = urls.map((el)=>el.url);
+    // let baseURL = "https://jsonplaceholder.typicode.com";
+    axios.post(baseURL,body).then((response)=>{
+        res.send(response.data);
+        console.log("post Data ",response.data);
+    })
+    .catch((err)=>{
+        if(err.response){
+        let {status,statusText} = err.response;
+        console.log(err.response);
+        res.status(status).send(statusText);
+        }
+        else {
+            res.status(404).send(err);
+        }
+    })
+})
